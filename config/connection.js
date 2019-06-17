0a48a9f56ef97b544602474a0d0f099f7fe10a41
Bill Brasher <wsbrasher1@msn.com>
2019-06-16 19:26:04 -0500

    added JAWS db information

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@
 var mysql = require("mysql");
 
 // MySQL DB Connection Information//
-var connection = mysql.createConnection({
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
     password: "kenmore71",
     database: "burgers_db"
   });
+};
 
 //To make the connection to MySQL//
 connection.connect(function(err){
